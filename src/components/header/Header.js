7b908423d4fd70067f9fc8b71e27aa6da45adb07
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-02-26 09:57:52 +0100

    finished the header comp

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,29 @@
+import Link from "next/link";
+
 const Header = () => {
     return (
-        <h1>
-            hello
-        </h1>
-    )
-}
+        <header className="bg-blend-color text-white py-4 shadow-lg">
+            <div className="container mx-auto flex justify-between items-center px-6">
+                <h1 className="text-2xl font-bold tracking-wide">
+                    Range & Rain
+                </h1>
+                <nav>
+                    <ul className="flex space-x-6">
+                        <li>
+                            <Link href="/" className="hover:text-gray-400">
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/about" className="hover:text-gray-400">
+                                About
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
+};
 
-export default Header
+export default Header;
