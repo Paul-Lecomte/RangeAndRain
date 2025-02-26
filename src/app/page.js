b51e1aca011ed7ba7963a51b938d7a17db56d50a
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-02-26 13:49:46 +0100

    changed the styling

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,19 @@
+'use client'
 import Form from '../components/input/InputForm'
 import Historic from '../components/distances_historic/DistanceHistory'
 
 export default function Home() {
-  return (
-      <div>
-          <Form />
-          <Historic />
-      </div>
-  );
+    return (
+        <div className="relative min-h-full p-4 flex flex-col sm:flex-row items-center justify-center sm:space-x-8">
+            {/* Form centered in the middle */}
+            <div className="flex justify-center items-center w-full max-w-3xl">
+                <Form />
+            </div>
+
+            {/* Logs positioned at the top-right on larger screens, stacked below on smaller screens */}
+            <div className="sm:absolute sm:top-4 sm:right-4 sm:w-80 sm:max-h-[calc(100vh-80px)] sm:max-w-[calc(35vh-80px)] sm:block flex items-center justify-center w-full">
+                <Historic />
+            </div>
+        </div>
+    );
 }
