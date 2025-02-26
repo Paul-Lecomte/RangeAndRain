6e7715eb5b2d2fcd1f9fe693e66c53bed7438222
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-02-26 09:57:27 +0100

    finished the footer comp

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,16 @@
 const Footer = () => {
     return (
-        <div>
-            <h1>
-                Footer
-            </h1>
-        </div>
-    )
-}
+        <footer className="bg-blend-color text-gray-400 text-xs py-2 text-center">
+            <p>&copy; {new Date().getFullYear()} Paul Lecomte • MIT License •
+                <a href="https://github.com/Paul-Lecomte/RangeAndRain"
+                   target="_blank"
+                   rel="noopener noreferrer"
+                   className="hover:text-white ml-1">
+                    GitHub
+                </a>
+            </p>
+        </footer>
+    );
+};
 
-export default Footer
+export default Footer;
