6caa7f3c777cbcb14955e3a7471db7da0f96880c
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-02-26 10:31:50 +0100

    imported the form and inputs

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,11 @@
+import Form from '../components/input/InputForm'
+import Historic from '../components/distances_historic/DistanceHistory'
 
 export default function Home() {
   return (
       <div>
-        main
+          <Form />
+          <Historic />
       </div>
   );
 }
