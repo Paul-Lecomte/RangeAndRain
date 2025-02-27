5f1b78dd243552f8d5be3bd77c79850d33d0ebd5
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-02-27 08:30:25 +0100

    also better centered the components

--- src/app/page.js
+++ src/app/page.js
@@ -4,15 +4,15 @@ import Historic from '../components/distances_historic/DistanceHistory'
 
 export default function Home() {
     return (
-        <div className="relative min-h-full p-4 flex flex-col sm:flex-row items-center justify-center sm:space-x-8">
-            {/* Form centered in the middle */}
-            <div className="flex justify-center items-center w-full max-w-3xl">
-                <Form />
+        <div className="flex flex-col sm:flex-row items-center justify-center relative p-4 h-full">
+            {/* Centered Form */}
+            <div className="flex justify-center items-center w-full max-w-3xl h-full">
+                <Form/>
             </div>
 
-            {/* Logs positioned at the top-right on larger screens, stacked below on smaller screens */}
-            <div className="sm:absolute sm:top-4 sm:right-4 sm:w-80 sm:max-h-[calc(100vh-80px)] sm:max-w-[calc(35vh-80px)] sm:block flex items-center justify-center w-full">
-                <Historic />
+            {/* Logs positioned at the top-right on large screens, below on smaller screens */}
+            <div className="sm:absolute sm:top-4 sm:right-4 sm:w-80 sm:max-h-[calc(100vh-80px)] sm:block w-full flex justify-center mt-6 sm:mt-0">
+                <Historic/>
             </div>
         </div>
     );
