b4bafafc18e69e9ac8e375b8f88fbe6d99929d1e
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-02-27 09:12:51 +0100

    created the strcuture

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,7 @@
 const Page = () => {
     return (
-        <div>
-            About
+        <div className="max-w-3xl mx-auto p-6 bg-gray-900 text-white rounded-lg">
+            <h1 className="text-4xl font-bold mb-6 text-center">About Artillery in Hell Let Loose</h1>
         </div>
     );
 };
